6024fa519afb533e2e320d4c90cfe48944b18320
tiaki1201 <tiaki1201@gmail.com>
2017-07-03 16:31:16 +1200

    test json

--- shop-around/search.js
+++ shop-around/search.js
@@ -3,14 +3,14 @@
  */
 
 function handleKey(e){
+    if(e.keyCode == 13){
+        search();
+    }
     console.log('test');
 }
 
-$(document).ready(function(e) {
+function search() {
 
-   // var APP_URL = "http://localhost:8080";
-    var APP_URL = "https://nwen304-group-project.herokuapp.com";
-    var error = console.error.bind(console);
     var taskName;
 
     taskName = $('#searchBar').val();
@@ -31,19 +31,28 @@ $(document).ready(function(e) {
         }
     })
 
-    function showSearch (row) {
-       // window.location = "https://nwen304-group-project.herokuapp.com";
-        console.log(JSON.stringify(row));
-        console.log(row);
-        var i = 0;
-        $('#searchItems').empty();
-        for(i; i<row.length; i++){
-            $('#searchItems').append('<h3>'+row[i].item_name+'</h3>');
-            $('#searchItems').append('<h3>'+row[i].item_price+'</h3>');
-            $('#searchItems').append('<h3>'+row[i].url+'</h3>');
+}
 
-        }
+function showSearch (row) {
+    // window.location = "https://nwen304-group-project.herokuapp.com";
+    console.log(JSON.stringify(row));
+    console.log(row);
+    var i = 0;
+    $('#searchItems').empty();
+    for(i; i<row.length; i++){
+        $('#searchItems').append('<h3>'+row[i].item_name+'</h3>');
+        $('#searchItems').append('<h3>'+row[i].item_price+'</h3>');
+        $('#searchItems').append('<h3>'+row[i].url+'</h3>');
 
     }
+
+}
+
+$(document).ready(function(e) {
+
+   // var APP_URL = "http://localhost:8080";
+    var APP_URL = "https://nwen304-group-project.herokuapp.com";
+    var error = console.error.bind(console);
+
 
 });

17ae6d3c0ed4ac24585290f003dd2093c52959ff
tiaki1201 <tiaki1201@gmail.com>
2017-07-04 19:02:32 +1200

    new

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ var port = process.env.PORT || 8080;
 var pg = require('pg');
 var bodyParser = require('body-parser');
 
-//var connectionString =
+var connectionString = "postgres://gpqmvhmwnsyjoe:sttng1abc@ec2-54-197-232-155.compute-1.amazonaws.com:5432/d1obt7gii4e3oh";
 
-//var client = new pg.Client(connectionString);
-//client.connect();
+var client = new pg.Client(connectionString);
+client.connect();
 
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
@@ -27,10 +27,35 @@ app.use(function (req, res, next) {
     next();
 });
 
-app.get('/', function (req, res){
-    console.log ('got get request\n');
-    res.send('Hello World!');
-});
+// Get request for search box
+app.post('/search', function (req, res) {
+
+    var keyword = req.body.task;
+    console.log(keyword);
+    console.log("flame" + keyword)
+
+    var query = client.query("SELECT * FROM collections WHERE LOWER (item_name) LIKE LOWER('%" + keyword + "%');");
+    var results = [];
+
+    query.on('error', function (err) {
+        console.log(err);
+    });
+
+    query.on('row',function(row){
+        results.push(row);
+    });
+
+    query.on('end',function() {
+        console.log(results.length + "Slade is gay");
+        res.send(results);
+    });
+
+
+    if(keyword.length<2){
+        console.log('nothing entered');
+        res.send('Nothing found');
+    }
+    });
 
 app.listen(port, function () {
     console.log('Example app listening on port 8080!');});
---
+++ shop-around/search.js
@@ -0,0 +1,58 @@
+/**
+ * Created by ntaylor on 27/06/17.
+ */
+var APP_URL = "https://nwen304-group-project.herokuapp.com";
+
+function handleKey(e){
+    if(e.keyCode == 13){
+        search();
+    }
+    console.log('test');
+}
+
+function search() {
+
+    var taskName;
+
+    taskName = $('#searchBar').val();
+
+    var taskone = {
+        task: taskName
+    };
+
+    console.log(taskone);
+
+    $.ajax({
+        method: 'post',
+        url: APP_URL + '/search',
+        dataType: 'json',
+        data: taskone,
+        success: function(data){
+            showSearch(data);
+        }
+    })
+
+}
+
+function showSearch (row) {
+    // window.location = "https://nwen304-group-project.herokuapp.com";
+    console.log(JSON.stringify(row));
+    console.log(row);
+    var i = 0;
+    $('#searchItems').empty();
+    for(i; i<row.length; i++){
+        $('#searchItems').append('<h3>'+row[i].item_name+'</h3>');
+        $('#searchItems').append('<h3>'+row[i].item_price+'</h3>');
+        $('#searchItems').append('<h3>'+row[i].url+'</h3>');
+
+    }
+
+}
+
+$(document).ready(function(e) {
+
+   // var APP_URL = "http://localhost:8080";
+    var error = console.error.bind(console);
+
+
+});
